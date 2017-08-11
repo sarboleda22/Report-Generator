53f848f7bef23efdc581effdbb6a631957b70c93
Sebastian Arboleda <sebasarboleda22@gmail.com>
2017-08-11 16:26:41 -0400

    Views added. API is created with flask_restful lib. Tested locally. Both GET and POST reqs work in ../slides/. | Models file was also created, it just handles a Slide class, which contains the object_id, name and content.

---
+++ src/models.py
@@ -0,0 +1,17 @@
+"""
+File used for defining all the models of the app
+"""
+from app import db
+
+class Slide(db.Model):
+	object_id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80))
+	content = db.Column(db.Text)
+
+	def __init__(self, name, content):
+		self.name = name
+		self.content = content
+
+	def __repr__(self):
+		return '<ID {} | User {}>'.format(str(self.object_id), self.name)
+
---
+++ src/views.py
@@ -0,0 +1,39 @@
+import requests
+from flask import render_template, url_for, request, redirect, jsonify, make_response
+from flask_restful import Resource
+from app import app, db, api
+from models import Slide
+
+
+class RestSlides(Resource):
+	#Handles the GET requests
+	def get(self):
+			response = {}
+			response['count'] = Slide.query.count() #Creates an item in the dict. with key 'count' and value <the number of rows in the database>
+			response['results'] = []	#creates another item in the dict w key 'results' and an empty list as the value
+			slides = Slide.query.all()	#the set of slides for iterating later
+			for i in slides:
+				results = {}
+				results['name'] = i.name
+				results['object_id'] = i.object_id
+				response['results'].append(results)
+			return response
+	#Handles the POST requests
+	def post(self):
+		#Use table 'slides'
+		__tablename__ = 'slides'
+
+		name = request.form['yourname']
+		content = request.form['youremail']
+		new_slide = Slide(name, content)
+		db.session.add(new_slide)
+		db.session.commit()
+		return 'New Slide Created | nombre : {} | content : {}'.format(name, content)
+
+#IN PROGRESS. THIS IS JUST A DUMMY FUNCTION.
+@app.route('/slides/<slide_id>')
+def presentation(slide_id):
+	return 'this is the slide {}'.format(slide_id)
+
+#Run the API in .../slides/
+api.add_resource(RestSlides, '/slides/')
