20f3b181da8a848a92f9264b89be2e8af695376e
Sebastian Arboleda <sebasarboleda22@gmail.com>
2017-08-11 16:18:54 -0400

    Created the app file, which should be run. The DataBase URI was added, in this case it points to a localhost. The Database object (db) is also created, and initialized before app.py is run

--- src/app.py
+++ src/app.py
@@ -1,1 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
 
+#App initialized in Flask
+app = Flask(__name__)
+api = Api(app)
+
+#Setting the track of modifications off
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#User info of the database, in this case, postgres
+db_info = {
+	'username':'hacknoob',
+	'password':'qwerty1234',
+	'host':'localhost',
+	'db_name':'prueba',
+}
+
+#Defining the URI of the db with the info given before. In this example, it is running on a localhost
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@{}/{}'.format(db_info['username'], db_info['password'], db_info['host'], db_info['db_name'])
+
+#Initializing the db object
+db = SQLAlchemy(app)
+
+from views import *
+
+if __name__ == '__main__':
+	db.create_all()
+	app.run(debug = True)
