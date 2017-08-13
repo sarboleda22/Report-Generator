9482acb34ead763cba83f50bdbcfa88f5c3e2be3
Sebastian Arboleda <sebasarboleda22@gmail.com>
2017-08-13 19:34:22 -0400

    Deleted dummy database credentials and added comment explaining how to manage the secret file

--- src/app.py
+++ src/app.py
@@ -9,13 +9,16 @@ api = Api(app)
 #Setting the track of modifications off
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#User info of the database, in this case, postgres
+"""
+User info of the database, in this case, postgres. Credentials are kept in a secret file, then imported in the form:
 db_info = {
-	'username':'hacknoob',
-	'password':'qwerty1234',
-	'host':'localhost',
-	'db_name':'prueba',
+	'username':'your_username',
+	'password':'your_usr_password',
+	'host':'your_host',
+	'db_name':'your_db_name',
 }
+"""
+from secrets import *
 
 #Defining the URI of the db with the info given before. In this example, it is running on a localhost
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@{}/{}'.format(db_info['username'], db_info['password'], db_info['host'], db_info['db_name'])
