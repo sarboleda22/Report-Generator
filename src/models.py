3ce75aaf187b438ab258b4f631d31af0ede48e33
Sebastian Arboleda <sebasarboleda22@gmail.com>
2017-08-13 18:02:38 -0400

    html template for the slideshow

--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ File used for defining all the models of the app
 from app import db
 
 class Slide(db.Model):
-	object_id = db.Column(db.Integer, primary_key=True)
+	object_id = db.Column(db.Integer, primary_key=True) #May need to add UUID for increased security
 	name = db.Column(db.String(80))
 	content = db.Column(db.Text)
 
